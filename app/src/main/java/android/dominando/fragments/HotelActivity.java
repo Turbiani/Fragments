cd9aeceea0973fe14a95298f468c01c11285426f
turbiani <leo.turbiani@gmail.com>
2016-04-23 20:40:33 -0300

    Adaptando layout para tablets

--- app/src/main/java/android/dominando/fragments/HotelActivity.java
+++ app/src/main/java/android/dominando/fragments/HotelActivity.java
@@ -1,6 +1,8 @@
 package android.dominando.fragments;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,8 +16,30 @@ public class HotelActivity extends AppCompatActivity implements AoClicarNoHotel{
 
     @Override
     public void clicouNoHotel(Hotel hotel) {
-        Intent it = new Intent(this, HotelDetalheActivity.class);
-        it.putExtra(HotelDetalheActivity.EXTRA_HOTEL, hotel);
-        startActivity(it);
+        if(isTablet()){
+            HotelDetalheFragment fragment = HotelDetalheFragment.novaInstancia(hotel);
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+
+            ft.replace(R.id.detalhe, fragment, HotelDetalheFragment.TAG_DETALHE);
+
+            ft.commit();
+        }
+
+        if(isSmartPhone()){
+            Intent it = new Intent(this, HotelDetalheActivity.class);
+            it.putExtra(HotelDetalheActivity.EXTRA_HOTEL, hotel);
+            startActivity(it);
+        }
     }
+
+    private boolean isTablet(){
+        return getResources().getBoolean(R.bool.tablet);
+    }
+
+    private boolean isSmartPhone(){
+        return getResources().getBoolean(R.bool.smartphone);
+    }
+
 }
