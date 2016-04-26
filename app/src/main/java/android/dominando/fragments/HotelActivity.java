19150a3f66272507a63663d7123cdee4bc9f5cf5
turbiani <leo.turbiani@gmail.com>
2016-04-25 23:04:31 -0300

    Inclusao da ActionBar com SearchVew

--- app/src/main/java/android/dominando/fragments/HotelActivity.java
+++ app/src/main/java/android/dominando/fragments/HotelActivity.java
@@ -3,24 +3,53 @@ package android.dominando.fragments;
 import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class HotelActivity extends AppCompatActivity implements AoClicarNoHotel{
+public class HotelActivity extends AppCompatActivity implements AoClicarNoHotel,
+        SearchView.OnQueryTextListener,
+        MenuItemCompat.OnActionExpandListener{
+
+    private FragmentManager mFragmentManager;
+    private HotelListFragment mListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
+
+        mFragmentManager = getSupportFragmentManager();
+        mListFragment = (HotelListFragment) mFragmentManager.findFragmentById(R.id.fragmentLista);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_hotel, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        searchView.setQueryHint(getString(R.string.hint_busca));
+        MenuItemCompat.setOnActionExpandListener(searchItem, this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void clicouNoHotel(Hotel hotel) {
         if(isTablet()){
             HotelDetalheFragment fragment = HotelDetalheFragment.novaInstancia(hotel);
 
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
+            FragmentTransaction ft = mFragmentManager.beginTransaction();
 
             ft.replace(R.id.detalhe, fragment, HotelDetalheFragment.TAG_DETALHE);
 
@@ -32,6 +61,25 @@ public class HotelActivity extends AppCompatActivity implements AoClicarNoHotel{
             it.putExtra(HotelDetalheActivity.EXTRA_HOTEL, hotel);
             startActivity(it);
         }
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return true;
+    }
+    @Override
+    public boolean onQueryTextChange(String s) {
+        mListFragment.buscar(s);
+        return false;
+    }
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        return true; // para expandir a view
+    }
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        mListFragment.limparBusca();
+        return true; // para voltar ao normal
     }
 
     private boolean isTablet(){
--- app/src/main/java/android/dominando/fragments/HotelListFragment.java
+++ app/src/main/java/android/dominando/fragments/HotelListFragment.java
@@ -1,10 +1,8 @@
 package android.dominando.fragments;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -25,6 +23,7 @@ public class HotelListFragment extends ListFragment {
         super.onActivityCreated(savedInstanceState);
 
         mHoteis = carregaHoteis();
+        limparBusca();
 
         mAdapter = new ArrayAdapter<Hotel>(getActivity()
                 , android.R.layout.simple_list_item_1
@@ -44,6 +43,38 @@ public class HotelListFragment extends ListFragment {
             AoClicarNoHotel listener = (AoClicarNoHotel)activity;
             listener.clicouNoHotel(hotel);
         }
+    }
+
+    public void buscar(String s) {
+        if (s == null || s.trim().equals("")) {
+            limparBusca();
+            return;
+        }
+        List<Hotel> hoteisEncontrados = new ArrayList<Hotel>(mHoteis);
+        for (int i = hoteisEncontrados.size() - 1; i >= 0; i--) {
+            Hotel hotel = hoteisEncontrados.get(i);
+            if (!hotel.nome.toUpperCase().contains(s.toUpperCase())) {
+                hoteisEncontrados.remove(hotel);
+            }
+        }
+        mAdapter = new ArrayAdapter<Hotel>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                hoteisEncontrados);
+        setListAdapter(mAdapter);
+    }
+
+    public void limparBusca() {
+        mAdapter = new ArrayAdapter<Hotel>(
+                getActivity(),
+                android.R.layout.simple_list_item_activated_1,
+                mHoteis);
+        setListAdapter(mAdapter);
+    }
+
+    public void adicionar(Hotel hotel) {
+        mHoteis.add(hotel);
+        mAdapter.notifyDataSetChanged();
     }
 
 
