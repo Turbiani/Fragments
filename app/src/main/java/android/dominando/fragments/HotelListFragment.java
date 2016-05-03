77284b27c03973d4c263c2750d51ba1d79da3667
turbiani <leo.turbiani@gmail.com>
2016-05-02 22:11:45 -0300

    Incluindo API de design para usar Snackbar no rollback de transações. Inclusão das funcionalidades de exclusão de multiplos itens selecionados em uma ListFragment

--- app/src/main/java/android/dominando/fragments/HotelListFragment.java
+++ app/src/main/java/android/dominando/fragments/HotelListFragment.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.util.SparseBooleanArray;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,15 +20,20 @@ import java.util.List;
 /**
  * Created by lcunha on 19/04/16.
  */
-public class HotelListFragment extends ListFragment {
+public class HotelListFragment extends ListFragment
+    implements ActionMode.Callback, AdapterView.OnItemLongClickListener{
+
+    ListView mListView;
     List<Hotel> mHoteis;
     ArrayAdapter<Hotel> mAdapter;
+    ActionMode mActionMode;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         mHoteis = carregaHoteis();
+        mListView = getListView();
         limparBusca();
 
         mAdapter = new ArrayAdapter<Hotel>(getActivity()
@@ -30,19 +41,83 @@ public class HotelListFragment extends ListFragment {
                 , mHoteis);
 
         setListAdapter(mAdapter);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mActionMode!=null){
+            iniciarModoExclusao();
+            atualizarTitulo();
+        }
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        Activity activity = getActivity();
-        if(activity instanceof AoClicarNoHotel){
-            Hotel hotel = (Hotel) l.getItemAtPosition(position);
+        if(mActionMode==null) {
+            Activity activity = getActivity();
+            if (activity instanceof AoClicarNoHotel) {
+                Hotel hotel = (Hotel) l.getItemAtPosition(position);
 
-            AoClicarNoHotel listener = (AoClicarNoHotel)activity;
-            listener.clicouNoHotel(hotel);
+                AoClicarNoHotel listener = (AoClicarNoHotel) activity;
+                listener.clicouNoHotel(hotel);
+            }
+        }else{
+            atualizarItensMarcados(mListView, position);
+            if(qtdeItensMarcados()==0){
+                mActionMode.finish();
+            }
         }
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        getActivity().getMenuInflater().inflate(R.menu.menu_delete_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem menuItem) {
+        if(menuItem.getItemId()==R.id.acao_delete){
+            SparseBooleanArray checked = mListView.getCheckedItemPositions();
+
+            for (int i = checked.size()-1; i >= 0; i--){
+                if(checked.valueAt(i)){
+                    mHoteis.remove(checked.keyAt(i));
+                }
+            }
+            mode.finish();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        mActionMode = null;
+        mListView.clearChoices();
+        mListView.setChoiceMode(ListView.CHOICE_MODE_NONE);
+        limparBusca();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        boolean consumed = (mActionMode==null);
+        if(consumed){
+            iniciarModoExclusao();
+            mListView.setItemChecked(i, true);
+            atualizarItensMarcados(mListView, i);
+        }
+
+        return consumed;
     }
 
     public void buscar(String s) {
@@ -57,6 +132,7 @@ public class HotelListFragment extends ListFragment {
                 hoteisEncontrados.remove(hotel);
             }
         }
+        mListView.setOnItemLongClickListener(null);
         mAdapter = new ArrayAdapter<Hotel>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
@@ -65,6 +141,7 @@ public class HotelListFragment extends ListFragment {
     }
 
     public void limparBusca() {
+        mListView.setOnItemLongClickListener(this);
         mAdapter = new ArrayAdapter<Hotel>(
                 getActivity(),
                 android.R.layout.simple_list_item_activated_1,
@@ -77,6 +154,36 @@ public class HotelListFragment extends ListFragment {
         mAdapter.notifyDataSetChanged();
     }
 
+
+    private void iniciarModoExclusao(){
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        mActionMode = activity.startSupportActionMode(this);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+
+    private void atualizarItensMarcados(ListView l, int position){
+        l.setItemChecked(position, l.isItemChecked(position));
+        atualizarTitulo();
+    }
+
+    private int qtdeItensMarcados() {
+        SparseBooleanArray checked = mListView.getCheckedItemPositions();
+        int checkedCount = 0;
+        for (int i = 0; i < checked.size(); i++) {
+            if (checked.valueAt(i)) {
+                checkedCount++;
+            }
+        }
+        return checkedCount;
+    }
+
+    private void atualizarTitulo() {
+        int checkedCount = qtdeItensMarcados();
+        String selecionados = getResources().getQuantityString(
+                R.plurals.numero_selecionados,
+                checkedCount, checkedCount);
+        mActionMode.setTitle(selecionados);
+    }
 
     private List<Hotel> carregaHoteis(){
         List<Hotel> hotels = new ArrayList<>();
